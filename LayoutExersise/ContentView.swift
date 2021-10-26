610868302dcd312effd3c07db8ec8c1dd88b98a2
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-10-26 14:51:52 -0400

    Initial Commit

---
+++ LayoutExersise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutExersise
+//
+//  Created by Noah Alexandre Soubliere on 2021-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutExersise/LayoutExersiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExersiseApp.swift
+//  LayoutExersise
+//
+//  Created by Noah Alexandre Soubliere on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExersiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
